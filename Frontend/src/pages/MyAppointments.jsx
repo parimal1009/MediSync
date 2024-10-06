9c7328ba544472b4cb81e5d2d0752eafaa0805c6
Onkar Mendhapurkar <61963755+onkar69483@users.noreply.github.com>
2024-10-06 15:07:52 +0000

    Added Frontend Pages
    Added Frontend Pages
        new file:   src/pages/About.jsx
        new file:   src/pages/Appointment.jsx
        new file:   src/pages/Contact.jsx
        new file:   src/pages/Doctors.jsx
        new file:   src/pages/Home.jsx
        new file:   src/pages/Login.jsx
        new file:   src/pages/MyAppointments.jsx
        new file:   src/pages/MyProfile.jsx

---
+++ Frontend/src/pages/MyAppointments.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MyAppointments = () => {
+  return (
+    <div>
+      <h1>Welcome to the My Appointments Page</h1>
+      <p>This is the My Appointments of our application.</p>
+    </div>
+  );
+};
+
+export default MyAppointments;
