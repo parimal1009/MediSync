15b6160ce9c628be71c717e729d44d4d3d0702e4
Onkar Mendhapurkar <61963755+onkar69483@users.noreply.github.com>
2024-10-07 09:14:09 +0000

    Added Navbar Header and basic pages setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,10 +8,12 @@ import MyProfile from "./pages/MyProfile";
 import Doctors from "./pages/Doctors";
 import Login from "./pages/Login";
 import Appointment from "./pages/Appointment";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <div className="mx-4 sm:mx-[10%]">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/doctors" element={<Doctors />} />
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
+import Header from "../components/Header";
 
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the home page of our application.</p>
+      <Header />
     </div>
   );
 };
