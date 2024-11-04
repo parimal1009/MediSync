4672df46769fcccd9fb9b264a6a7961e68b989ab
onkar69483 <onkar69483@gmail.com>
2024-11-04 17:37:56 +0530

    Create Dashboard Page for admin panel

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -9,6 +9,7 @@ const AdminContextProvider = (props) => {
     const [aToken, setAToken] = useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'')
     const [doctors, setDoctors] = useState([])
     const [appointments, setAppointments] = useState([])
+    const [dashData, setDashData] = useState(false)
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
@@ -70,6 +71,21 @@ const AdminContextProvider = (props) => {
         }
     }
 
+    const getDashData = async () => {
+        try {
+            const {data} = await axios.get(backendUrl+'/api/admin/dashboard', {headers: {aToken}})
+            if (data.success) {
+                setDashData(data.dashData)
+                console.log(data.dashData);
+                
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
     const value = {
         aToken, setAToken,
         backendUrl,
@@ -79,7 +95,9 @@ const AdminContextProvider = (props) => {
         appointments,
         setAppointments,
         getAllAppointments,
-        cancelAppointment
+        cancelAppointment,
+        dashData,
+        getDashData
     }
 
     return (
