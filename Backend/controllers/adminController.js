f47b6aabafd36172ac81f711312f22c7391a44ac
onkar69483 <onkar69483@gmail.com>
2024-11-04 17:37:39 +0530

    Backend Fix: reverse() function

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -161,7 +161,7 @@ const adminDashboard = async(req,res) => {
       doctors : doctors.length,
       appointments:appointments.length,
       patients : users.length,
-      latestAppointments : appointments.reverse.slice(0,5)
+      latestAppointments : appointments.reverse().slice(0,5)
     }
     res.json({success:true,dashData})
   }catch(error){
