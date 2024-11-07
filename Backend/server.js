381555942528fb95fa77cd9f4a6574e2485f50ae
onkar69483 <onkar69483@gmail.com>
2024-11-08 00:30:07 +0530

    version 1: basic chatting related to doctor suggestion and other, upload image and text extraction using vision ocr

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,7 @@ import connectCloudinary from "./config/cloudinary.js";
 import adminRouter from "./routes/adminRoute.js";
 import doctorRouter from "./routes/doctorRoute.js";
 import userRouter from "./routes/userRoute.js";
+import router from "./routes/ocrRoute.js"; 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -23,6 +24,7 @@ app.use(cors({
 app.use("/api/admin", adminRouter);
 app.use("/api/doctor", doctorRouter);
 app.use("/api/user", userRouter);
+app.use("/api", router);
 
 app.get("/", (req, res) => {
   res.send("Hello from the backend!");
