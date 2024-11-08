9d74c041b1637feef258979fa4fe03c096e027ab
Jay Trivedi <jayutrivedi1214@gmail.com>
2024-11-08 16:26:21 +0530

    Added payment page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,26 +10,28 @@ import Login from "./pages/Login";
 import Appointment from "./pages/Appointment";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import PaymentPage from "./pages/PaymentPage"; 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function App() {
   return (
     <div className="mx-4 sm:mx-[10%]">
-    <ToastContainer/>
+      <ToastContainer />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/doctors" element={<Doctors />} />
-        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path="/doctors/:speciality" element={<Doctors />} />
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/my-profile" element={<MyProfile />} />
         <Route path="/my-appointments" element={<MyAppointments />} />
         <Route path="/appointment/:docId" element={<Appointment />} />
+        <Route path="/payment" element={<PaymentPage />} /> 
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
